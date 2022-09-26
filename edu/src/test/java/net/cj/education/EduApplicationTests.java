40a9958663031efed60b859ca12933bd84b82443
최창원 <changwon.choi@cj.net>
2022-09-26 15:14:46 +0900

    첫번째 커밋

---
+++ edu/src/test/java/net/cj/education/EduApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cj.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
